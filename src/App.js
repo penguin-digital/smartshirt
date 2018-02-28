4af23ca7037204eeb3be5ef035f80805ea656440
Dan Atanasov <danmanm@gmail.com>
2018-02-28 12:38:00 +0200

    Add svg icons

--- src/App.js
+++ src/App.js
@@ -6,21 +6,30 @@ import Home from './containers/Home'
 import Settings from './containers/Settings'
 import Blockly from './containers/Blockly'
 
+// icons
+import menu from './assets/icons/menu.svg'
+
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Welcome to React</h1>
+        <div className="App overflow-hidden">
+          <header className="App-header ph2 pv3 cf">
+            <Link to="/">
+              <h2 className="App-title fl ma0 black f3">SmartShirt</h2>
+            </Link>
+
+            <img src={menu} alt="Menu" className="fr mt2 pointer" />
           </header>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/blockly" component={Blockly} />
-          </Switch>
+          <div className="ph2 vh-100">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/settings" component={Settings} />
+              <Route path="/blockly" component={Blockly} />
+            </Switch>
+          </div>
         </div>
       </Router>
     )
