a52739ec5cdeae6b172495ea0f469fbe5db75fcc
Iva Kop <iva@penguin.digital>
2018-03-01 13:09:46 +0200

    Input fields function

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -6,36 +6,94 @@ import snap from '../assets/icons/snap.svg'
 import wink from '../assets/icons/wink.svg'
 import smile from '../assets/icons/smile.svg'
 
-const Settings = () => (
-  <div className="relative container">
-    <div className="f2 fw6 pv5">Laufschrift</div>
-    <input type="text" label="Custom text" className="w-80 h2" />
-    <div className="pv2"> Custom text</div>
-    <div className="apply fw6 mt4 mb4 white f4 tc">Apply</div>
-    <div className="f3 pv4">Helligkeit</div>
-    <input id="myRange" type="range" className="w-90" />
-    <div className="f3 pv4">Farbe</div>
-    <input id="input-1" type="range" className="w-90 mt3" />
-    <div className="f4 pv2">Rot: 155</div>
-    <input id="input-2" type="range" className="w-90 mt3" />
-    <div className="f4 pv2">Grun:0</div>
-    <input id="input-3" type="range" className="w-90 mt3" />
-    <div className="f4 pv2">Brau:0</div>
-    <div className="colors">
-      <div className="bg-yellow w-20 h2 mr3 mv4" />
-      <div className="bg-blue w-20 h2 mr3 mv4" />
-      <div className="bg-purple w-20 h2 mr3 mv4" />
-    </div>
-    <div className="f2 pv4 fw6"> Emoticons </div>
-    <img className="w3 pr2 mb3" src={smile} alt="" />
-    <img className="w3 pr2 mb3" src={wink} alt="" />
-    <img className="w3 pr2 mb3" src={bla} alt="" />
-    <img className="w3 pr2 mb3" src={sad} alt="" />
+export default class Settings extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: 'Custom text',
+      range1: 0,
+      range2: 0,
+      range3: 0,
+      range4: 0
+    }
+  }
 
-    <div className="f2 pv4 fw6"> Social Media </div>
-    <img className="w3 pr3 mb6" src={snap} alt="" />
-    <img className="w3 mb6" src={fb} alt="" />
-  </div>
-)
+  render() {
+    return (
+      <div className="relative container">
+        <div className="f2 fw6 pv5">Laufschrift</div>
+        <input
+          type="text"
+          id="customText"
+          label="Custom text"
+          className="w-80 h2"
+          onChange={() =>
+            this.setState({ text: document.getElementById('customText').value })
+          }
+        />
+        <div className="pv2"> {this.state.text}</div>
 
-export default Settings
+        <div className="apply fw6 mt4 mb4 white f4 tc">Apply</div>
+
+        <div className="f3 pv4">Helligkeit</div>
+        <input
+          id="range1"
+          type="range"
+          className="w-90"
+          defaultValue="0"
+          onChange={() =>
+            this.setState({ range1: document.getElementById('range1').value })
+          }
+        />
+        <div className="f4 pv2">Value: {this.state.range1}</div>
+        <div className="f3 pv4">Farbe</div>
+        <input
+          id="range2"
+          type="range"
+          className="w-90 mt3"
+          defaultValue="0"
+          onChange={() =>
+            this.setState({ range2: document.getElementById('range2').value })
+          }
+        />
+        <div className="f4 pv2">Rot: {this.state.range2}</div>
+        <input
+          id="range3"
+          type="range"
+          className="w-90 mt3"
+          defaultValue="0"
+          onChange={() =>
+            this.setState({ range3: document.getElementById('range3').value })
+          }
+        />
+        <div className="f4 pv2">Grun: {this.state.range3}</div>
+        <input
+          id="range4"
+          type="range"
+          className="w-90 mt3"
+          defaultValue="0"
+          onChange={() =>
+            this.setState({ range4: document.getElementById('range4').value })
+          }
+        />
+        <div className="f4 pv2">Brau: {this.state.range4}</div>
+
+        <div className="colors">
+          <div className="bg-yellow w-20 h2 mr3 mv4" />
+          <div className="bg-blue w-20 h2 mr3 mv4" />
+          <div className="bg-purple w-20 h2 mr3 mv4" />
+        </div>
+
+        <div className="f2 pv4 fw6"> Emoticons </div>
+        <img className="w3 pr2 mb3" src={smile} alt="" />
+        <img className="w3 pr2 mb3" src={wink} alt="" />
+        <img className="w3 pr2 mb3" src={bla} alt="" />
+        <img className="w3 pr2 mb3" src={sad} alt="" />
+
+        <div className="f2 pv4 fw6"> Social Media </div>
+        <img className="w3 pr3 mb6" src={snap} alt="" />
+        <img className="w3 mb6" src={fb} alt="" />
+      </div>
+    )
+  }
+}
